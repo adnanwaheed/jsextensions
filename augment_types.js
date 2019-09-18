35c98d58a32a0aa7b0bd0a7b180992e1c6c663dd
Adnan Waheed <643114+adnanwaheed@users.noreply.github.com>
2019-09-18 11:37:34 +0200

    Update augment_types.js

--- augment_types.js
+++ augment_types.js
@@ -21,14 +21,14 @@ Promise.method('timeout', function(ms) {
 });
 
 Promise.any = function(promises) {
-	if (!promises || promises.length<1)
-		return Promise.reject("empty list");
-	// There is no way to know which promise is rejected.
-	// So we map it to a new promise to return the index when it fails
-	let index_promises = promises.map((p, index) => p.catch(() => {throw index;}));
-	return Promise.race(index_promises).catch( index => {
+    if (!promises || promises.length<1)
+        return Promise.reject("empty list");
+    // There is no way to know which promise is rejected.
+    // So we map it to a new promise to return the index when it fails
+    let index_promises = promises.map((p, index) => p.catch(() => {throw index;}));
+    return Promise.race(index_promises).catch( index => {
         // remove the failed promise
         promises.splice(index, 1)[0];
         return Promise.any(promises);
-	} )
+    } )
 }
