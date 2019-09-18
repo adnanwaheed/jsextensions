1b71ef9e651238806f40771c65c4b0ebe45a842d
Adnan Waheed <643114+adnanwaheed@users.noreply.github.com>
2019-09-18 11:36:53 +0200

    Added Promise.any

--- augment_types.js
+++ augment_types.js
@@ -19,4 +19,16 @@ Promise.method('timeout', function(ms) {
     });
     return Promise.race([this, timeout_promise]);
 });
-                    
+
+Promise.any = function(promises) {
+	if (!promises || promises.length<1)
+		return Promise.reject("empty list");
+	// There is no way to know which promise is rejected.
+	// So we map it to a new promise to return the index when it fails
+	let index_promises = promises.map((p, index) => p.catch(() => {throw index;}));
+	return Promise.race(index_promises).catch( index => {
+        // remove the failed promise
+        promises.splice(index, 1)[0];
+        return Promise.any(promises);
+	} )
+}
